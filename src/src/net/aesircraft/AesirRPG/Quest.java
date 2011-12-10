cda494c1dd32893f976627e9da119639bdb41a9a
TheEliteFour <xeology@aesircraft.net>
2011-12-10 06:08:29 -0500

    Finished IO, mostly done with Questers.  Tweaked Quests.

--- src/src/net/aesircraft/AesirRPG/Quest.java
+++ src/src/net/aesircraft/AesirRPG/Quest.java
@@ -3,6 +3,7 @@ package net.aesircraft.AesirRPG;
 import java.util.List;
 import org.bukkit.inventory.ItemStack;
 
+
 public class Quest
 {
 	private List<String> description;
@@ -12,7 +13,7 @@ public class Quest
 	private List<String> objectives;
 	private List<String> rewardDescription;
 	private boolean repeatable;
-	private boolean loaded=false;
+	private boolean loaded = false;
 	private int rewardExp;
 	private double rewardEyrir;
 	private int requiredExp;
@@ -32,206 +33,332 @@ public class Quest
 	private List<Byte> blockDestroyData;
 	private List<Integer> blockDestroyCounts;
 	private List<ItemStack> requiredItems;
-	
-	public void update(){
+
+	public void update()
+	{
 		Filer.update(this);
 	}
-	public void load(String name){
+
+	public void load(String name)
+	{
 		Filer.load(this);
 	}
-	public void save(){
+
+	public void save()
+	{
 		Filer.newQuest(this);
 	}
-	
-	public String getTitle(){
+
+	public String getTitle()
+	{
 		return title;
 	}
-	public String getRequires(){
+
+	public String getRequires()
+	{
 		return requires;
 	}
-	public String getRequiredClass(){
+
+	public String getRequiredClass()
+	{
 		return requiredClass;
 	}
-	public int getHeroesExp(){
+
+	public int getHeroesExp()
+	{
 		return heroesExp;
 	}
-	public List<String> getDescription(){
+
+	public List<String> getDescription()
+	{
 		return description;
 	}
-	public List<String> getCompletionMessage(){
+
+	public List<String> getCompletionMessage()
+	{
 		return completionMessage;
 	}
-	public List<ItemStack> getCollectItems(){
+
+	public List<ItemStack> getCollectItems()
+	{
 		return collectItems;
 	}
-	public List<String> getObjectives(){
+
+	public List<String> getObjectives()
+	{
 		return objectives;
 	}
-	public List<String> getRewardDescription(){
+
+	public List<String> getRewardDescription()
+	{
 		return rewardDescription;
 	}
-	public boolean isRepeatable(){
+
+	public boolean isRepeatable()
+	{
 		return repeatable;
 	}
-	public int getRewardExp(){
+
+	public int getRewardExp()
+	{
 		return rewardExp;
 	}
-	public boolean isLoaded(){
+
+	public boolean isLoaded()
+	{
 		return loaded;
 	}
-	public int getRequiredExp(){
+
+	public int getRequiredExp()
+	{
 		return requiredExp;
 	}
-	public double getRewardEyrir(){
+
+	public double getRewardEyrir()
+	{
 		return rewardEyrir;
 	}
-	public double getRequiredEyrir(){
+
+	public double getRequiredEyrir()
+	{
 		return requiredEyrir;
 	}
-	public List<String> getCompleters(){
+
+	public List<String> getCompleters()
+	{
 		return completers;
 	}
-	public List<String> getKills(){
+
+	public List<String> getKills()
+	{
 		return kills;
 	}
-	public List<Integer> getKillCounts(){
+
+	public List<Integer> getKillCounts()
+	{
 		return killCounts;
 	}
-	public List<String> getQuesters(){
+
+	public List<String> getQuesters()
+	{
 		return questers;
 	}
-	public List<ItemStack> getRewardItems(){
+
+	public List<ItemStack> getRewardItems()
+	{
 		return rewardItems;
 	}
-	public List<ItemStack> getRequiredItems(){
+
+	public List<ItemStack> getRequiredItems()
+	{
 		return requiredItems;
 	}
-	public List<Integer> getBlocks(){
+
+	public List<Integer> getBlocks()
+	{
 		return blocks;
 	}
-	public List<Byte> getBlockDatas(){
+
+	public List<Byte> getBlockDatas()
+	{
 		return blockData;
 	}
-	public List<Integer> getBlockCounts(){
+
+	public List<Integer> getBlockCounts()
+	{
 		return blockCounts;
 	}
-	public List<Integer> getBlockDestroys(){
+
+	public List<Integer> getBlockDestroys()
+	{
 		return blockDestroys;
 	}
-	public List<Byte> getBlockDestroyData(){
+
+	public List<Byte> getBlockDestroyData()
+	{
 		return blockDestroyData;
 	}
-	public List<Integer> getBlockDestroyCounts(){
+
+	public List<Integer> getBlockDestroyCounts()
+	{
 		return blockDestroyCounts;
 	}
-	public void setTitle(String string){
-		title=string;
+
+	public void setTitle(String string)
+	{
+		title = string;
 	}
-	public void setRequires(String string){
-		requires=string;
+
+	public void setRequires(String string)
+	{
+		requires = string;
 	}
-	public void addKills(String string, int amount){
+
+	public void addKills(String string, int amount)
+	{
 		kills.add(string);
 		killCounts.add(amount);
 	}
-	public void removeKills(int index){
+
+	public void removeKills(int index)
+	{
 		kills.remove(index);
 		killCounts.remove(index);
 	}
-	public void addCollectItem(ItemStack item){
+
+	public void addCollectItem(ItemStack item)
+	{
 		collectItems.add(item);
 	}
-	public void removeCollectItem(int index){
+
+	public void removeCollectItem(int index)
+	{
 		collectItems.remove(index);
 	}
-	public void addCompletionMessage(String string){
+
+	public void addCompletionMessage(String string)
+	{
 		completionMessage.add(string);
 	}
-	public void removeCompletionMessage(int index){
+
+	public void removeCompletionMessage(int index)
+	{
 		completionMessage.remove(index);
 	}
-	public void addDescription(String string){
+
+	public void addDescription(String string)
+	{
 		description.add(string);
 	}
-	public void addObjectives(String string){
+
+	public void addObjectives(String string)
+	{
 		objectives.add(string);
 	}
-	public void addRewardDescription(String string){
+
+	public void addRewardDescription(String string)
+	{
 		rewardDescription.add(string);
 	}
-	public void removeDescription(int index){
+
+	public void removeDescription(int index)
+	{
 		description.remove(index);
 	}
-	public void removeObjectives(int index){
+
+	public void removeObjectives(int index)
+	{
 		objectives.remove(index);
 	}
-	public void removeRewardDescription(int index){
+
+	public void removeRewardDescription(int index)
+	{
 		rewardDescription.remove(index);
 	}
-	public void setRequiredClass(String string){
-		requiredClass=string;
+
+	public void setRequiredClass(String string)
+	{
+		requiredClass = string;
 	}
-	public void setHeroesExp(int exp){
-		heroesExp=exp;
+
+	public void setHeroesExp(int exp)
+	{
+		heroesExp = exp;
 	}
-	public void setLoaded(boolean bool){
-		loaded=bool;
+
+	public void setLoaded(boolean bool)
+	{
+		loaded = bool;
 	}
-	public void setRepeatable(boolean bool){
-		repeatable=bool;
+
+	public void setRepeatable(boolean bool)
+	{
+		repeatable = bool;
 	}
-	public void setRewardExp(int exp){
-		rewardExp=exp;
+
+	public void setRewardExp(int exp)
+	{
+		rewardExp = exp;
 	}
-	public void setRewardEyrir(int eyrir){
-		rewardEyrir=eyrir;
+
+	public void setRewardEyrir(int eyrir)
+	{
+		rewardEyrir = eyrir;
 	}
-	public void setRequiredExp(int exp){
-		requiredExp=exp;
+
+	public void setRequiredExp(int exp)
+	{
+		requiredExp = exp;
 	}
-	public void setRequiredEyrir(int eyrir){
-		requiredEyrir=eyrir;
+
+	public void setRequiredEyrir(int eyrir)
+	{
+		requiredEyrir = eyrir;
 	}
-	public void addCompleter(String string){
+
+	public void addCompleter(String string)
+	{
 		completers.add(string);
 	}
-	public void addQuester(String string){
+
+	public void addQuester(String string)
+	{
 		questers.add(string);
-	}
-	public void removeCompleter(int index){
+	}	
+	
+	public void removeCompleter(int index)
+	{
 		completers.remove(index);
 	}
-	public void removeQuester(int index){
+
+	public void removeQuester(int index)
+	{
 		questers.removeAll(blocks);
 	}
-	public void addRewardItem(ItemStack item){
+
+	public void addRewardItem(ItemStack item)
+	{
 		rewardItems.add(item);
 	}
-	public void addRequiredItem(ItemStack item){
+
+	public void addRequiredItem(ItemStack item)
+	{
 		requiredItems.add(item);
 	}
-	public void removeRewardItem(int index){
+
+	public void removeRewardItem(int index)
+	{
 		rewardItems.remove(index);
 	}
-	public void removeRequiredItem(int index){
+
+	public void removeRequiredItem(int index)
+	{
 		requiredItems.remove(index);
 	}
-	public void addBlock(int id,byte data,int count){
+
+	public void addBlock(int id, byte data, int count)
+	{
 		blocks.add(id);
 		blockData.add(data);
 		blockCounts.add(count);
 	}
-	public void removeBlock(int index){
+
+	public void removeBlock(int index)
+	{
 		blocks.remove(index);
 		blockData.remove(index);
 		blockCounts.remove(index);
 	}
-	public void addBlockDestroy(int id,byte data,int count){
+
+	public void addBlockDestroy(int id, byte data, int count)
+	{
 		blockDestroys.add(id);
 		blockDestroyData.add(data);
 		blockDestroyCounts.add(count);
 	}
-	public void removeBlockDestroy(int index){
+
+	public void removeBlockDestroy(int index)
+	{
 		blockDestroys.remove(index);
 		blockDestroyData.remove(index);
 		blockDestroyCounts.remove(index);
--- src/src/net/aesircraft/AesirRPG/Quester.java
+++ src/src/net/aesircraft/AesirRPG/Quester.java
@@ -1,27 +1,417 @@
 package net.aesircraft.AesirRPG;
 
+import java.util.HashMap;
+import java.util.List;
+import net.aesircraft.TrueEconomy.Data.Bank;
+import net.aesircraft.TrueEconomy.Data.Form;
+import net.aesircraft.TrueEconomy.Data.Players;
+import net.aesircraft.TrueEconomy.Exchange;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 
 public class Quester
 {
 	private Player player;
+	private String quest;
+	private boolean loaded = false;
+	private HashMap<ItemStack, Integer> collectItems;
+	private HashMap<String, Integer> kills;
+	private List<Integer> blocks;
+	private List<Byte> blockData;
+	private List<Integer> blockCounts;
+	private List<Integer> blockDestroys;
+	private List<Byte> blockDestroyData;
+	private List<Integer> blockDestroyCounts;
 	
-	public void load(Player player){
-		this.player=player;
+	
+	public boolean hasMetRequirements(){
+		Quest questa=new Quest();
+		questa.load(quest);
+		if (questa.getBlocks()!=null){
+			List<Integer> blocksa=questa.getBlockCounts();
+			for (int ctr=0;ctr<blocksa.size();ctr++){
+				if (blocksa.get(ctr)>blockCounts.get(ctr))
+					return false;
+			}
+		}
+		if (questa.getBlockDestroys()!=null){
+			List<Integer> blockDestroysa=questa.getBlockCounts();
+			for (int ctr1=0;ctr1<blockDestroysa.size();ctr1++){
+				if (blockDestroysa.get(ctr1)>blockDestroyCounts.get(ctr1))
+					return false;
+			}
+		}
+		
+		if (questa.getKills()!=null){
+			List<String> killsa=questa.getKills();
+			List<Integer> killsc=questa.getKillCounts();
+			for (int ctr2=0;ctr2<killsa.size();ctr2++){
+				if (kills.get(killsa.get(ctr2).toLowerCase())>killsc.get(ctr2))
+					return false;
+			}
+		}
+		
+		if (questa.getCollectItems()!=null){
+			List<ItemStack> items=questa.getCollectItems();
+			for (int ctr3=0;ctr3<items.size();ctr3++){
+				if (items.get(ctr3).getAmount()>getCollectItemProgress(items.get(ctr3)))
+					return false;
+			}
+		}
+		
+		if (questa.getRequiredItems()!=null){
+			List<ItemStack> items=questa.getRequiredItems();
+			for (int ctr4=0;ctr4<items.size();ctr4++){
+				if (!InventoryWorkaround.containsItem(player.getInventory(), true,items.get(ctr4)))
+					return false;
+			}
+		}
+		
+		if (questa.getRequiredExp()!=0){
+			if (getExp()<questa.getRewardExp())
+				return false;			
+		}
+		
+		if (questa.getRequiredEyrir()!=0){
+			if (getEyrir()<questa.getRequiredEyrir())
+				return false;			
+		}
+		
+		return true;
+		
 	}
 	
-	public Player getPlayer(){
+	public double getEyrir(){
+		return Players.get(player.getName().toLowerCase());
+	}
+	public void subtractEyrir(double amount){
+		Players.sub(player.getName().toLowerCase(),amount);
+		Bank.add(amount);
+	}
+	public void addEyrir(double amount){
+		if (Bank.locked()){
+			player.sendMessage("§4You were unable to recieve the amount of §b"+Form.form(amount)+"§4 because the bank is locked down!");
+			return;
+		}
+		Players.add(player.getName().toLowerCase(),amount);
+		player.sendMessage("§2For completing this quest you recieved §b"+Form.form(amount)+"§2!");
+		Bank.sub(amount);
+		Exchange.tax(player,amount);
+	}
+	
+	public boolean isCollectingItems(){
+		if (collectItems==null)
+			return false;
+		return true;
+	}
+	public boolean isCollectingKills(){
+		if (kills==null)
+			return false;
+		return true;
+	}
+	public boolean isCollectingblocks(){
+		if (blocks==null)
+			return false;
+		return true;
+	}
+	public boolean isCollectingBlockDestroys(){
+		if (blockDestroys==null)
+			return false;
+		return true;
+	}
+	
+	public boolean doingCollectItem(ItemStack item)
+	{
+		ItemStack stack = new ItemStack(item.getTypeId(), item.getData().getData());
+		if (collectItems.containsKey(stack))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean doingKills(String kill)
+	{
+		if (kills.containsKey(kill.toLowerCase()))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public void complete()
+	{
+		Quest questa = new Quest();
+		questa.load(quest);
+		questa.addCompleter(player.getName());		
+		questa.update();
+		Filer.complete(this);
+	}
+	
+	public void setOnQuest(Quest quest)
+	{
+		Filer.newPQuest(this, quest);
+		quest.addQuester(player.getName().toLowerCase());
+	}
+	
+	public int getCollectItemProgress(ItemStack item)
+	{
+		ItemStack stack = new ItemStack(item.getTypeId(), item.getData().getData());
+		if (collectItems.containsKey(stack))
+		{
+			return collectItems.get(stack);
+		}
+		return 0;
+	}
+	
+	public int getKillsProgress(String kill)
+	{
+		if (kills.containsKey(kill.toLowerCase()))
+		{
+			return kills.get(kill.toLowerCase());
+		}
+		return 0;
+	}
+	
+	public void addACollectItem(ItemStack item)
+	{
+		ItemStack stack = new ItemStack(item.getTypeId(), item.getData().getData());
+		if (collectItems.containsKey(stack))
+		{
+			int amount = collectItems.get(stack);
+			collectItems.remove(stack);
+			collectItems.put(stack, amount + item.getAmount());
+		}
+	}
+	
+	public void addAKill(String kill)
+	{		
+		if (kills.containsKey(kill.toLowerCase()))
+		{
+			int amount = kills.get(kill.toLowerCase());
+			kills.remove(kill.toLowerCase());
+			kills.put(kill.toLowerCase(), amount + 1);
+		}
+	}
+	
+	public boolean doingBlock(int id, byte data)
+	{
+		for (int ctr = 0; ctr < blocks.size(); ctr++)
+		{
+			if (blocks.get(ctr) == id)
+			{
+				if (blockData.get(ctr) == data)
+				{
+					return true;
+				}
+			}			
+		}
+		return false;
+	}
+	
+	public boolean doingBlockDestroy(int id, byte data)
+	{
+		for (int ctr = 0; ctr < blockDestroys.size(); ctr++)
+		{
+			if (blockDestroys.get(ctr) == id)
+			{
+				if (blockDestroyData.get(ctr) == data)
+				{
+					return true;
+				}
+			}			
+		}
+		return false;
+	}
+	
+	public int getBlockProgress(int id, byte data)
+	{
+		for (int ctr = 0; ctr < blocks.size(); ctr++)
+		{
+			if (blocks.get(ctr) == id)
+			{
+				if (blockData.get(ctr) == data)
+				{
+					return blockCounts.get(ctr);
+				}
+			}			
+		}
+		return 0;
+	}
+	
+	public int getBlockDestroyProgress(int id, byte data)
+	{
+		for (int ctr = 0; ctr < blockDestroys.size(); ctr++)
+		{
+			if (blockDestroys.get(ctr) == id)
+			{
+				if (blockDestroyData.get(ctr) == data)
+				{
+					return blockDestroyCounts.get(ctr);
+				}
+			}			
+		}
+		return 0;
+	}
+	
+	public void addABlock(int id, byte data)
+	{
+		for (int ctr = 0; ctr < blocks.size(); ctr++)
+		{
+			if (blocks.get(ctr) == id)
+			{
+				if (blockData.get(ctr) == data)
+				{
+					blockCounts.set(ctr, blockCounts.get(ctr) + 1);
+				}
+			}			
+		}
+	}
+	
+	public void addABlockDestroy(int id, byte data)
+	{
+		for (int ctr = 0; ctr < blockDestroys.size(); ctr++)
+		{
+			if (blockDestroys.get(ctr) == id)
+			{
+				if (blockDestroyData.get(ctr) == data)
+				{
+					blockDestroyCounts.set(ctr, blockDestroyCounts.get(ctr) + 1);
+				}
+			}			
+		}
+	}
+	
+	public void addBlocks(int id, byte data, int count)
+	{
+		blocks.add(id);
+		blockData.add(data);
+		blockCounts.add(count);
+	}
+	
+	public void setQuest(String title)
+	{
+		quest = title;
+	}
+	
+	public void addBlockDestroys(int id, byte data, int count)
+	{
+		blockDestroys.add(id);
+		blockDestroyData.add(data);
+		blockDestroyCounts.add(count);
+	}
+	
+	public void addCollectItem(ItemStack item, int amount)
+	{
+		collectItems.put(item, amount);
+	}
+	
+	
+	public String getHeroClass(){
+		if (AesirRPG.heroes)
+		return AesirRPG.hero.getHeroManager().getHero(player).getHeroClass().getName();
+		return "";
+	}
+	
+	public boolean isHeroClass(String name){
+		if (AesirRPG.heroes)
+		return (AesirRPG.hero.getHeroManager().getHero(player).getHeroClass().getName().toLowerCase().equals(name.toLowerCase()));
+		return false;
+	}
+	
+	public void addHeroExp(double amount){
+		if (AesirRPG.heroes)
+		AesirRPG.hero.getHeroManager().getHero(player).addExp(amount, AesirRPG.hero.getHeroManager().getHero(player).getHeroClass());
+	}
+	
+	public int getExp(){
+		return player.getTotalExperience();
+	}
+	
+	public void giveExp(int amount){
+		for (int expctr=0;expctr<amount;expctr++){
+				player.giveExp(1);
+			}
+	}
+	
+	public boolean canQuest(Quest quest){
+		String requiredQuest;
+		String hClass;		
+		if (quest.getRequiredClass()!=null && AesirRPG.heroes){
+		hClass=quest.getRequiredClass().toLowerCase();
+		if (!isHeroClass(hClass))
+			return false;
+		}
+		if (quest.getRequires()!=null){
+		requiredQuest=quest.getRequires();
+		if (!completed(requiredQuest))
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean completed(String name){
+		Quest quest=new Quest();
+		quest.load(name);
+		if (quest.isLoaded()){
+			List<String> list=quest.getCompleters();
+			for (int ctr=0;ctr<list.size();ctr++){
+				if (list.get(ctr).toLowerCase().equals(player.getName().toLowerCase()))
+					return true;
+			}
+		}
+		return false;
+			
+	}
+	
+	public void subtractExp(int amount){
+		int exp=player.getTotalExperience()-amount;			
+			player.setExp(0);
+			player.setTotalExperience(0);
+			player.setLevel(0);
+			for (int expctr=0;expctr<exp;expctr++){
+				player.giveExp(1);
+			}
+	}
+	
+	public void addKills(String name, int amount)
+	{
+		kills.put(name.toLowerCase(), amount);
+	}
+
+	public void setLoaded(boolean bool)
+	{
+		loaded = bool;
+	}
+	
+	public void set(Player player)
+	{
+		this.player = player;
+	}
+	
+	public void load()
+	{
+		Filer.load(this);
+	}
+	
+	public Player getPlayer()
+	{
 		return player;
 	}
-	public String getQuest(){
-		String quest="";
-		
+
+	public String getQuest()
+	{
 		return quest;
 	}
+
+	public boolean isLoaded()
+	{
+		return loaded;
+	}
 	
-	public boolean onQuest(){
-		boolean bool=false;
-		return bool;
+	public boolean onQuest()
+	{
+		return Filer.exists(this);
 	}
 }
